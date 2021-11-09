41f56fb2d5b5863fd2e5dde3b4829d47bb2aeef9
srinivasplato <67679734+srinivasplato@users.noreply.github.com>
2021-11-09 17:01:43 +0530

    script-changes
    script-changes

--- application/controllers/Script.php
+++ application/controllers/Script.php
@@ -20,14 +20,65 @@ class Script extends CI_Controller {
 
 		foreach($receipts as $key=>$value){
 
-			if($key  <= 10){
-			    $this->common_model->save_pdf($value['receipt_id'],'');
-			  }
+
+			//if($key  <= 10){
+			    $this->save_pdf($value['receipt_id'],'');
+			 // }
+
 		}
 
 		
 
 	}
+
+
+
+	public function save_pdf($receipt_id,$previous_payment){ 
+         $path=''; $full_path='';
+
+         //load mPDF library
+         $this->load->library('M_pdf'); 
+         //now pass the data//
+         $data=array();
+        
+         $data['payment_data']= $this->getStudentPaymentRecord($receipt_id);
+         $data['student_data']['course_name']=$data['payment_data']['course_name'];
+         $data['student_data']['batch_name']=$data['payment_data']['batch_name'];
+
+          //echo '<pre>';print_r($data['student_data']);exit;
+
+         $html=".html";
+         $receipt_id.$html =$this->load->view('admin/paymentportal/payment_nonbhatia_pdf',$data, true); //load the pdf.php by passing our data and get all data in $html varriable.
+          //echo '<pre>';print_r($data);
+
+         //$var_html=$receipt_id.'_html';
+         $pdfFilePath = $receipt_id.".pdf"; 
+         $this->m_pdf->pdf->WriteHTML($receipt_id.$html);
+
+            //download it D save F.
+         
+         $this->m_pdf->pdf->Output("./storage/paymentreceipts/".$pdfFilePath, "F");
+         $path= "storage/paymentreceipts/".$pdfFilePath;
+         $full_path=base_url().$path;
+         //unset($var_html);
+         unset($this->m_pdf);
+         
+         
+
+         $update_path=array('receipt_pdf_path' => $path);
+         $this->db->update('student_payment_details',$update_path,array('receipt_id' => $receipt_id));
+
+         
+         
+         }
+
+         public function getStudentPaymentRecord($receipt_id){
+
+         	$query="SELECT stp.*,b.batch_name,c.course_name FROM tbl_student_payment_details stp INNER JOIN tbl_courses c on c.id=stp.course_id INNER JOIN tbl_batchs b on b.id=stp.batch_id where stp.receipt_id='$receipt_id' ";
+         	$res=$this->db->query($query)->row_array();
+         	return $res;
+         }
+
 
 
 
